f6d7985b5f38c619387d0db827ef4c4153fb191c
moaath ibrahim <moaaz@gmail.com>
2024-10-10 06:20:22 -0700

    edit on header and add burger menu

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ const deleteButton = document.querySelector(".delete-button");
 const pOne = document.querySelector(".pOne");
 const pTwo = document.querySelector(".pTwo");
 
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
+
+// Toggle burger menu and navigation links
+burger.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+    burger.classList.toggle('open');
+});
+
+
+
 submitButton.addEventListener('click', function(){
     let age = Number(inputForm.value);
 
